2d460eb60d33f4bd84b02f7ead59dc15dfbdf153
Abdelhak El Gadyry <ael-gady@e3r1p1.1337.ma>
2025-10-07 08:45:21 +0100

    Static min & max functions

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -171,26 +171,25 @@ Fixed	Fixed::operator--(int) // Post-decrement
 
 // Static min/max functions :
 
-Fixed&		Fixed::min(Fixed &a, Fixed &b)
+Fixed& Fixed::min(Fixed &a, Fixed &b)
 {
-
+	return (a < b ? a : b);
 }
 
-const Fixed&	Fixed::min(const Fixed &a, const Fixed &b)
+const Fixed& Fixed::min(const Fixed &a, const Fixed &b)
 {
-
+	return (a < b ? a : b);
 }
 
-Fixed&		Fixed::max(Fixed &a, Fixed &b)
+Fixed& Fixed::max(Fixed &a, Fixed &b)
 {
-
+	return (a > b ? a : b);
 }
 
-const Fixed&	Fixed::max(const Fixed &a, const Fixed &b)
+const Fixed& Fixed::max(const Fixed &a, const Fixed &b)
 {
-
+	return (a > b ? a : b);
 }
-
 
 
 std::ostream& operator<<(std::ostream &os, const Fixed &f)
