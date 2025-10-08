551f5c66cb2519ab12918ed23f6d5bbaed997a00
Abdelhak El Gadyry <ael-gady@e3r1p1.1337.ma>
2025-10-08 02:15:53 +0100

    remove printing the msg in constractors ...

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -2,48 +2,35 @@
 
 const int Fixed::fractionBits = 8;
 
-Fixed::Fixed() : rawBits(0)
-{
-	std::cout << "Default constructor called" << std::endl;
-}
+Fixed::Fixed() : rawBits(0) {}
 
 Fixed::Fixed(const int nbr)
 {
-	std::cout << "Integer constructor called" << std::endl;
 	this->rawBits = nbr << this->fractionBits;// 2⁸ = 256
 }
 
 Fixed::Fixed(const float nbr)
 {
-	std::cout << "Float constructor called" << std::endl;
 	this->rawBits = roundf(nbr * (1 << this->fractionBits));
 }
 
 
 Fixed::Fixed(const Fixed& other)
 {
-	std::cout << "Copy constructor called" << std::endl;
-
 	// this->rawBits = other.rawBits;
 	*this = other;
 }
 
 Fixed& Fixed::operator=(const Fixed& other)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
-
 	this->rawBits = other.rawBits;
 	return (*this);
 }
 
-Fixed::~Fixed()
-{
-	std::cout << "Destructor called" << std::endl;
-}
+Fixed::~Fixed() {}
 
 int Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (this->rawBits);
 }
 
